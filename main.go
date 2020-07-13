509abe34b1e5e3b791f3eadb42d169d13cf2afec
fu-l <fu-l@klab.com>
2020-07-13 12:20:53 +0800

    modify limits

--- main.go
+++ main.go
@@ -8,16 +8,18 @@ import (
 	"log"
 	"os"
 	"sync"
+	"syscall"
 )
 
 const (
 	OFFICAL_URL = "https://kubernetes-charts.storage.googleapis.com/index.yaml"
-	MAXFILE     = 2048
 )
 
 func main() {
+	max := GetOpenFiles().Cur * 2 / 3
+	log.Printf("Current Chan Size is %d\n", max)
 	var wg sync.WaitGroup
-	maxChan := make(chan bool, MAXFILE)
+	maxChan := make(chan bool, max)
 	I := chart.Index{}
 	if err := yaml.Unmarshal(fetch.FetchIndexYaml(OFFICAL_URL), &I); err != nil {
 		log.Fatalf("Unmarshal struct %s Failed, Error is %s\n", I, err)
@@ -39,4 +41,15 @@ func main() {
 		log.Fatalf("Write Index.yaml Failed, Error is %s\n", err)
 	}
 
+}
+
+func GetOpenFiles() syscall.Rlimit {
+	var rlimit syscall.Rlimit
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit)
+	if err != nil {
+		log.Fatalf("Get limit Error: %s\n", err)
+	}
+	log.Printf("Current Open Files Count is: %d\n", rlimit.Cur)
+	log.Println("If you want to speed up download, Increase number by using `ulimit -n` command")
+	return rlimit
 }
